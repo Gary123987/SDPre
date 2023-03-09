29dc216c612fa778f69d98e8449a105dee0f7ec2
Gary Fultz <fultz.gary@gmail.com>
2023-03-09 00:57:02 -0500

    Second labs upload

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
+    for (int b = 10; b >= 0; b--) {
+      System.out.println(b);
+
+    }
+
+  }
+}
